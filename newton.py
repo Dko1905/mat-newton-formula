2fe09cf8acaad9eef87f502ad86efa3886c30681
Daniel Florescu <Daniel.florescu1905@protonmail.ch>
2021-01-15 09:53:07 +0100

    Fixed not showing result, and added comments to code.

--- newton.py
+++ newton.py
@@ -1,11 +1,13 @@
-# Calculate future numbers using Newtons formula.
+# Predict value n using Newtons formula.
 
+# Calculate difference between two numbers.
 def diff(a, b):
 	if a > b:
 		return a - b
 	else:
 		return b - a
 
+# Predict value n using formula.
 def calc(input, n):
 	t = input[0] + input[1] * n
 	for x in range(len(input) - 2):
@@ -18,6 +20,7 @@ def calc(input, n):
 		t += top/div
 	return t
 
+# Calculate values for formula by creating a tree.
 def calc_tree(input):
 	output = []
 	output.append(input[0])
@@ -27,6 +30,7 @@ def calc_tree(input):
 		output.append(input[0])
 	return output
 
+# Get inputs, and show result.
 def main():
 	input_array = input("Enter array of the numbers in one line, without spaces. (0,1,2,3): ").split(',')
 	input_array = [int(numeric_string) for numeric_string in input_array]
@@ -34,7 +38,8 @@ def main():
 	print("Calculated variables are", input_variables)
 	n = int(input("Please write the number you want to get. (n): "))
 	result = calc(input_variables, n)
-	print("Result is ", n)
+	print("Result is ", result)
 
+# Python specific code.
 if __name__ == "__main__":
 	main()
